4106821f43d84f11a913e4f7b6b4c261e063bcf5
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-01 17:55:19 -0500

    general commit

--- search.js
+++ search.js
@@ -1,1 +1,13 @@
 // this file contains the js to make the search bar work 
+ const searchInput = document.querySelector('.input');
+ const clearButton = document.getElementById('clear');
+
+ searchInput.addEventListener ("input", (event) => {
+    let searchValue = event.target.value;
+    //checks if input exists and if it is larger than 0 
+    if (searchValue && searchValue.trim().length > 0 ){ 
+        searchValue = searchValue.trim().toLowerCase()
+    } else {
+    }
+
+ })
